959a623c15e672f736baccfeca57b9f5c6434b27
brianjoseph1990 <brianjoseph1990@gmail.com>
2017-06-20 17:47:06 -0400

    Update test.case.generator.R

--- test.cases/test.case.generator.R
+++ test.cases/test.case.generator.R
@@ -1,5 +1,8 @@
 ######
 ###### scripts to generate test cases for barcode demultiplexing challenge
+list.of.packages <- c("data.table", "stringr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 library(data.table)
 library(stringr)
 
